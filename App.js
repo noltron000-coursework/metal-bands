933671b2def359d8e99dc2d81055c81a17abdc13
Nolan Kovacik <noltron000@gmail.com>
2019-05-16 13:49:03 -0700

    setting up folder structure

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Index from './components/index.js'
 
-export default class App extends React.Component {
+
+class App extends React.Component {
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
-			</View>
-		);
+			<Index />
+		)
 	}
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
+export default App
